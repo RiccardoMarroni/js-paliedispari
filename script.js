e44c7790aea3865ab9d32e1ad9d9a6af589dd58d
RiccardoMarroni <marroni.riccardo@gmail.com>
2024-03-14 13:46:07 +0100

    pari o dispari

--- script.js
+++ script.js
@@ -19,3 +19,9 @@ if(palindroma(parola)){
 } else {
 	console.log(parola + " non è una parola palindroma.");
 }
+
+// Gioco Pari e dispari:
+// L’utente sceglie pari o dispari e un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1
+// a 5) per il computer.
+// Sommiamo i due numeri e dichiariamo chi ha vinto
